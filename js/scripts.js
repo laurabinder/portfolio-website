34fe431098c1bd6663f4563d92883b17c2ef0522
laurabinder <74289152+laurabinder@users.noreply.github.com>
2021-10-08 18:53:39 +0200

    Add burger menu and  smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,119 +1,19 @@
-// -------------------------------------------------------------------------------
-// Task 1:
-// -------------------------------------------------------------------------------
-
-function validate(userInput) {
-	if (!userInput) {
-		window.alert("You haven't wrote anything!");
-	}
-	else if (userInput === "Bobby") {
-		window.alert("You can't use Bobby!");
-		return false;
-	}
-	else if (userInput.length > 20) {
-		window.alert("You can only enter 20 characters!");
-		return false;
-	}
-	else {
-		console.log(userInput);
-		return true;
-	}
-	console.log(userInput);
-
-// DON'T CHANGE ANYTHING BELOW THIS LINE!
-
-
-function submitFunction() {
-  validate(document.getElementById("emailInput").value);
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
 
-document.getElementById("submit-btn").addEventListener("click", function(event){
-  event.preventDefault()
+$('a[href^="#"]').on('click',function(e) {
+ e.preventDefault();
+ var target = this.hash;
+ var $target = $(target);
+ $('html, body').stop().animate({
+  'scrollTop': $target.offset().top
+ }, 900, 'swing', function () {
+  window.location.hash = target;
+ });
 });
-
-
-
-// -------------------------------------------------------------------------------
-// Task 2 (Part 1)
-// -------------------------------------------------------------------------------
-
-function checkNumber(value) {
-  if (value >= 0 && value <= 99) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// DONT CHANGE ANYTHING BELOW THIS LINE!
-
-function addToOutput(text) {
-  var output = document.getElementById("output");
-  output.innerHTML = output.innerHTML += "<br>" + text;
-}
-
-document.getElementById("start").onclick = function() {
-  for(var i = 0; i < 100; i++) {
-    if(checkNumber(i)) {
-      addToOutput(i);
-    }
-  }
-}
-
-
-// -------------------------------------------------------------------------------
-// Task 2 (Part 2)
-// -------------------------------------------------------------------------------
-
-function checkNumber(value) {
-  if (
-    value === 0 ||
-    (value >= 10 && value <= 20) ||
-    (value >= 42 && value <= 43) ||
-    (value >= 60 && value <= 80) ||
-    value > 90
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// DONT CHANGE ANYTHING BELOW THIS LINE!
-
-function addToOutput(text) {
-  var output = document.getElementById("output");
-  output.innerHTML = output.innerHTML += "<br>" + text;
-}
-
-document.getElementById("start").onclick = function() {
-  for(var i = 0; i < 100; i++) {
-    if(checkNumber(i)) {
-      addToOutput(i);
-    }
-  }
-}
-
-// -------------------------------------------------------------------------------
-// Task 3:
-// -------------------------------------------------------------------------------
-
-
-function startButtonClicked()
-{
-  for (var i = 100; i >= 100 && i <= 200; i++)
-  {
-  console.log(i);
-  }
-}
-
-// DONT CHANGE ANYTHING BELOW THIS LINE!
-
-function textOutput(text) {
-  var output = document.getElementById("output");
-  output.innerHTML = output.innerHTML += "<br>" + text;
-}
-
-document.getElementById("start").onclick = function() {
-  startButtonClicked();
-}
